369729ed29c190e3cd356f9a0bf298447a4dc337
1-b 22ms <114579548+freedomfrade@users.noreply.github.com>
2022-10-03 11:20:43 +0000

    zd

---
+++ KartaPracy1.py
@@ -0,0 +1,37 @@
+# Zad 1
+
+# a = int(input())
+# b = int(input())
+# print(a**2 + b**2)
+
+# Zad 2
+
+# a = int(input())
+# b = int(input())
+# print(a + b)
+
+# # Zad 3
+
+# a = int(input())
+# b = int(input())
+# print((a + b) ** 2)
+
+# # Zad 4
+
+# a = int(input())
+# b = int(input())
+# print((a - b) ** 3)
+
+# Zad 5
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+
+# print(a ** b ** c)
+
+# Zad 5
+
+a, b, c = map(int(input().split())
+
+print (a * b * c)
